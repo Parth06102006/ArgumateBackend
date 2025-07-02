d93f5a5e8580fa25571a4ed7ba7883f2dddbd409
Parth <parth.6106garg@gmail.com>
2025-07-02 22:16:28 +0530

    feat: Add topic classification route and reorganize speech routes

--- src/routes/speech.route.js
+++ src/routes/speech.route.js
@@ -1,4 +1,4 @@
-import { createSpeech,voiceToText,createPoiQues,createPoiAns,sentimentalAnalysis } from "../controllers/speech.controller.js";
+import { createSpeech,voiceToText,createPoiQues,createPoiAns,sentimentalAnalysis,topicClassifcation, } from "../controllers/speech.controller.js";
 import { authHandling } from "../middlewares/auth.middleware.js";
 import { Router } from "express";
 import {upload} from '../middlewares/multer.middleware.js'
@@ -6,9 +6,10 @@ import {upload} from '../middlewares/multer.middleware.js'
 const router = Router();
 
 router.route('/create/speech/:id').post(authHandling,createSpeech);
-router.route('/create/voice').post(upload.single('audio'),voiceToText);
-router.route('/create/sentimentalAnalysis').post(sentimentalAnalysis);
 router.route('/create/poiQues/:id').post(authHandling,createPoiQues);
 router.route('/create/poiAns/:id').post(authHandling,createPoiAns);
+router.route('/create/voice').post(upload.single('audio'),voiceToText);
+router.route('/create/sentimentalAnalysis').post(sentimentalAnalysis);
+router.route('/create/topicClassify/:id').post(authHandling,topicClassifcation)
 
 export default router;
