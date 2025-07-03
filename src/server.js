1d21d9fc63ff3c0d3793bf0ed6182c4745b1ef01
Parth <parth.6106garg@gmail.com>
2025-07-03 17:11:43 +0530

    feat: Add notes router and improve CORS configuration in server setup

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import userRouter from './routes/user.route.js'
 import debateRouter from './routes/debate.route.js'
 import speechRouter from './routes/speech.route.js'
 import scoreRouter from './routes/score.route.js'
+import notesRouter from './routes/notes.route.js'
 import { errorHandling } from './middlewares/error.middleware.js';
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -20,13 +21,13 @@ const PORT = process.env.PORT || 3000
 const io = new Server(server,
     {
         cors:{
-        origin:process.env.FRONTEND_URL,
+        origin:`${process.env.FRONTEND_URL}`,
         credentials:true
 
     }
 }
 );
-
+console.log('Frontend Link',process.env.FRONTEND_URL)
 io.on('connection',(socket)=>{
     console.log('a user connected');
 
@@ -43,7 +44,8 @@ io.on('connection',(socket)=>{
     })
 })
 app.use(express.json())
-app.use(cors({origin:process.env.FRONTEND_URL,
+app.use(cors({origin:`${process.env.FRONTEND_URL}`,
+    secure:false,
     credentials:true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
@@ -55,15 +57,13 @@ app.use('/api/v1',userRouter);
 app.use('/api/v1',debateRouter);
 app.use('/api/v1',speechRouter);
 app.use('/api/v1',scoreRouter);
+app.use('/api/v1',notesRouter);
 app.use(errorHandling)
 export {io}
 
 dbConnect()
 .then(()=>{
-    if(process.env.NODE_ENV!=='production')
-    {
-        server.listen(PORT,()=>{console.log(`Server is listening at the ${PORT}`)})
-    }
+    server.listen(PORT,()=>{console.log(`Server is listening at the ${PORT}`)})
 })
 .catch(()=>{
     console.log('Something went wrong')
